aea938a3980546a8c4aa57d6cfa919dc3391636c
kazuma takiuchi <basstrombone413@gmail.com>
2023-12-11 06:26:13 +0900

    generatorページ追加

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,6 +1,11 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
     <footer className="bg-gray-200">
+      <div className="p-2 text-s flex justify-center">
+        <Link href="generator">Generator</Link>
+      </div>
       <p className="p-2 text-center text-xs">
         Copyright © 2019 Webty, inc. All Rights Reserved.
       </p>
