571bec8340573a5e6b853372d4bafc976a9a68bf
kazuma takiuchi <basstrombone413@gmail.com>
2023-12-11 22:43:59 +0900

    header list linktool追加

--- components/ArticleEditor/ArticleEditor.tsx
+++ components/ArticleEditor/ArticleEditor.tsx
@@ -1,8 +1,33 @@
 import EditorJS from "@editorjs/editorjs";
+import Header from "@editorjs/header";
+// @ts-ignore
+import List from "@editorjs/list";
+// @ts-ignore
+import LinkTool from "@editorjs/link";
 
 const ArticleEditor = () => {
-  const editor = new EditorJS({
+  new EditorJS({
     holder: "editor",
+    tools: {
+      header: {
+        // @ts-ignore
+        class: Header,
+        shortcut: "CMD+SHIFT+H",
+        config: {
+          placeholder: "へッダー",
+          levels: [1, 2, 3, 4],
+          defaultLevel: 3,
+        },
+        inlineToolbar: ["link"],
+      },
+      list: {
+        class: List,
+        inlineToolbar: true,
+      },
+      linkTool: {
+        class: LinkTool,
+      },
+    },
   });
   return <div id="editor" />;
 };
